be4e5e6fe8b8cc8370db9577faa80c313a24f151
Евгений Шабунин <Shabunin@2b.group>
2020-09-18 18:18:59 +0700

    Initial commit

---
+++ src/main/java/com/test/currency_converter/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.test.currency_converter.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+}
